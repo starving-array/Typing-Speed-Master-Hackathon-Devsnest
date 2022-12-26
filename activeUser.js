5ac79171a22482a2b8d38e91c12e688b6d81c277
starving-array <gowork.archis@gmail.com>
2022-12-26 19:19:27 +0530

    flat icon added

--- activeUser.js
+++ activeUser.js
@@ -1,4 +1,7 @@
 let activeUser = localStorage.getItem("activeUser") || null;
+let userDatabase = JSON.parse(localStorage.getItem("userDatabase")) || [];
+let leaderBoardDatabase = JSON.parse(localStorage.getItem("leaderBoard")) || [];
+
 console.log(activeUser);
 // activeUser = "Archie"
 let account = document.getElementById("account") || null;
@@ -6,7 +9,7 @@ let signbutton = document.getElementById("signbutton") || null;
 let leaderBoard = document.getElementById("leaderBoard") || null;
 
 if (activeUser != null) {
-  account.innerText = activeUser;
+  account.innerText = activeUser.username;
   signbutton.style.display = "none";
   leaderBoard.style.display = "block";
 }
@@ -19,20 +22,57 @@ let recordFormat = {
 };
 
 let accountForm = {
+  mobile: "",
   username: "",
   password: "",
 };
 
-let form = document.getElementById("signinform") || null;
-if (form != null) {
-  form.addEventListener("submit", function (e) {
-    e.preventDefault();
-    console.log(e);
+// sign in
+let signinForm = document.getElementById("signinform");
+signinForm.addEventListener("submit", function (e) {
+  signinAccount(e);
+});
+function signinAccount(e) {
+  e.preventDefault();
+  let username = document.getElementById("mobileSignIn");
+  let password = document.getElementById("passwordSignIn");
+  let signinState = false;
+  userDatabase.forEach(function (elem) {
+    if (username.value == elem.mobile && password.value == elem.password) {
+      localStorage.setItem("activeUser", elem);
+      alert("Account logged in");
+      window.location.reload();
+    }
   });
+  console.log(e);
 }
-function createAccount(name, password) {
-  let sevenDays = JSON.parse(localStorage.getItem("sevenDaysData")) || []; //array
-  localStorage.setItem("location", JSON.stringify(locationStoreByCity));
+
+// sign up
+let signupForm = document.getElementById("signupform");
+signinForm.addEventListener("submit", function (e) {
+  createAccount(e);
+});
+function createAccount(e) {
+  console.log(e)
+  e.preventDefault();
+  let mobile = document.getElementById("mobile");
+  let username = document.getElementById("username");
+  let password = document.getElementById("password");
+  accountForm.mobile = mobile.value;
+  accountForm.username = username.value;
+  accountForm.password = password.value;
+
+  let signinState = false;
+  userDatabase.forEach(function (elem) {
+    if (mobile.value == elem.mobile) {
+      signinState = true;
+    }
+  });
+  userDatabase.append(accountForm);
+  alert("Account created");
+  if (!signinState) {
+    localStorage.setItem("userDatabase", JSON.stringify(userDatabase));
+  }
 }
 
 function DataPushToLocalServer(userid, Data) {}
